2a2c5dacb080dfe9424a292a8e2cfbfde0093f7a
davids91 <toth.david.munka@gmail.com>
2020-05-17 09:33:46 +0200

    Added missing boilerplate license to some files

--- cxx/test/src/cost_function_mse_test.cc
+++ cxx/test/src/cost_function_mse_test.cc
@@ -10,7 +10,6 @@
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *    GNU General Public License for more details.
  *
- *
  *    You should have received a copy of the GNU General Public License
  *    along with Rafko.  If not, see <https://www.gnu.org/licenses/> or
  *    <https://github.com/davids91/rafko/blob/master/LICENSE>
