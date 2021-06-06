ff8b560b8dd40ac0c09ed7d6ee310b411b9b915e
David Toth <toth.david.munka@gmail.com>
2021-06-06 21:27:38 +0200

    Added a data pool to handle buffer access from multiple threads safely

---
+++ src/main/cxx/sparse_net_library/models/data_pool.h
@@ -0,0 +1,73 @@
+/*! This file is part of davids91/Rafko.
+ *
+ *    Rafko is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Rafko is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with Rafko.  If not, see <https://www.gnu.org/licenses/> or
+ *    <https://github.com/davids91/rafko/blob/master/LICENSE>
+ */
+
+#ifndef DATA_POOL_H
+#define DATA_POOL_H
+
+#include "rafko_global.h"
+
+#include <vector>
+#include <deque>
+#include <mutex>
+#include <algorithm>
+
+namespace sparse_net_library{
+
+using std::vector;
+using std::deque;
+using std::mutex;
+
+/**
+ * @brief      This data container allocates buffers on-demand and handles
+ *              data acess. The container is thread-safe.
+ */
+template<class T>
+class DataPool{
+public:
+  DataPool(uint32 pool_size, uint32 expected_buffer_size)
+  : buffer_pool(pool_size, vector<T>())
+  {
+    std::for_each(buffer_pool.begin(),buffer_pool.end(),[=](vector<T>& buf){
+      buf.reserve(expected_buffer_size);
+    });
+  }
+
+  DataPool() = default;
+
+  vector<T>& reserve_buffer(uint32 number_of_elements){
+    std::lock_guard<mutex> my_lock(buffers_mutex);
+  	for(uint32 buffer_index = 0; buffer_index < buffer_pool.size();++buffer_index){
+  		if(0u == buffer_pool[buffer_index].size()){ /* if the vector[buffer_index] has 0 elements --> the vector is free */
+  			buffer_pool[buffer_index].resize(number_of_elements); /* reserve the vector */
+  			return buffer_pool[buffer_index]; /* and make it available */
+  		}
+  	}
+  	buffer_pool.push_back(vector<T>(number_of_elements));
+  	return buffer_pool.back();
+  }
+
+  void release_buffer(vector<T>& buffer){
+  	buffer.resize(0);
+  }
+
+private:
+  deque<vector<T>> buffer_pool;
+  mutex buffers_mutex;
+};
+
+} /* namespace sparse_net_library */
+#endif /* DATA_POOL_H */
---
+++ src/main/cxx/test/src/data_pool_test.cc
@@ -0,0 +1,80 @@
+/*! This file is part of davids91/Rafko.
+ *
+ *    Rafko is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Rafko is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with Rafko.  If not, see <https://www.gnu.org/licenses/> or
+ *    <https://github.com/davids91/rafko/blob/master/LICENSE>
+ */
+
+#include "test/catch.hpp"
+#include "test/test_utility.h"
+#include <vector>
+#include <thread>
+
+#include "rafko_mainframe/models/service_context.h"
+#include "sparse_net_library/models/data_pool.h"
+
+namespace sparse_net_library_test {
+
+using std::vector;
+using std::thread;
+
+using sparse_net_library::DataPool;
+
+/*###############################################################################################
+ * Testing if data pool allocates data with the correct parameters
+ * */
+TEST_CASE("Data Pool parameters", "[data-handling][data-pool]"){
+  uint32 variants = 100;
+  DataPool<sdouble32> data_pool;
+  for(uint32 i = 0; i < variants; ++i){
+    uint32 buffer_size = rand()%500;
+    vector<sdouble32>& buffer = data_pool.reserve_buffer(buffer_size);
+    REQUIRE( buffer.size() == buffer_size );
+    data_pool.release_buffer(buffer);
+  }
+}
+
+/*###############################################################################################
+ * Testing if data pool is thread-safe
+ * */
+void use_buffer_thread(DataPool<sdouble32>& data_pool){
+  uint32 buffer_size = rand()%500;
+  vector<sdouble32>& buffer = data_pool.reserve_buffer(buffer_size);
+  vector<sdouble32> test_buffer = vector<sdouble32>(buffer_size);
+  for(uint32 i = 0; i < buffer_size; ++i){
+    test_buffer[i] = rand()%1000;
+    buffer[i] = test_buffer[i];
+  }
+  for(uint32 i = 0; i < buffer_size; ++i){
+    REQUIRE( test_buffer[i] == buffer[i] );
+  }
+  data_pool.release_buffer(buffer);
+}
+
+TEST_CASE("Data Pool multi-thread access", "[data-handling][data-pool][multi-threading]"){
+  uint32 variants = 10;
+  DataPool<sdouble32> data_pool;
+  for(uint32 i = 0; i < variants; ++i){
+    vector<thread> threads = vector<thread>();
+    for(uint32 j = 0; j < variants; ++j){
+      threads.push_back(thread(&use_buffer_thread, std::ref(data_pool)));
+    }
+    while(0 < threads.size())
+      if(threads.back().joinable()){
+        threads.back().join();
+        threads.pop_back();
+      }
+  }
+}
+
+} /* namespace sparse_net_library_test */
